ed43eea3bfd511fe8085af2559990f3a73e34d0a
romeoDjoman <romeo.djoman@outlook.fr>
2025-02-09 01:40:39 +0100

    feat: ajout du menu e-commerce et traduction en français

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { UserStorageService } from '../../services/storage/user-storage.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  title = 'Inscic | Shop & for Journals';
 
+  isCustomerLoggedIn: boolean = UserStorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = UserStorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
+      this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+    })
+  }
+
+  logout() {
+    UserStorageService.signOut();
+    this.router.navigateByUrl('login');
+  }
 }
